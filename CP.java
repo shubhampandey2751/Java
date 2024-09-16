331e063030195677bec6d8a2beab7affc76751b5
Shubham Pandey <144239086+shubhampandey2751@users.noreply.github.com>
2024-09-16 11:44:11 +0530

    Add files via upload

---
+++ CP.java
@@ -0,0 +1,16 @@
+package Examquestion;
+import java.util.*;
+public class CP {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=new String("SHUBHAM");
+        char ch=str.charAt(2);
+        System.out.println(ch);
+      
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+        
+
+    }
+    
+}
