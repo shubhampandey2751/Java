4febc52f56d4967fb103c3584bae873b2742fe4f
Shubham Pandey <144239086+shubhampandey2751@users.noreply.github.com>
2024-09-16 11:40:34 +0530

    Add files via upload

---
+++ Yield.java
@@ -0,0 +1,31 @@
+package Excercise;
+class thread1 extends Thread{
+    public void run(){
+        String name=Thread.currentThread().getName();
+        for(int i=1;i<=3;i++){
+            System.out.println(name);
+            Thread.yield();
+        }
+    }
+}
+class thread2 extends Thread{
+    public void run(){
+        String name=Thread.currentThread().getName();
+        for(int i=1;i<=3;i++){
+            System.out.println(name);
+          
+        }
+    }
+
+}
+public class Yield {
+    public static void main(String[] args) {
+     thread1 t1=new thread1();
+     thread2 t2=new thread2();
+     t1.setName("SHUBHAM");
+     t2.setName("PANDEY");
+     t1.start();
+     t2.start();;   
+    }
+}
+// String name=Thread.currentName().getname()
