454b4f95ffa252b40d02ca8880cbb3f212e7b928
Shubham Pandey <144239086+shubhampandey2751@users.noreply.github.com>
2024-09-16 11:40:17 +0530

    Add files via upload

---
+++ string2.java
@@ -0,0 +1,84 @@
+package Excercise;
+
+import java.util.Scanner;
+public class string2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        /*String s=new String("SHUBHAM")
+        System.out.println(s);
+        String name="Shubham";
+        System.out.println(name);
+        String st=sc.next();
+        System.out.println(st);
+        /* */
+
+        /*String a="ANKIT";
+        System.out.println(a);
+
+        String b="ANKIT";
+        System.out.println(b);
+
+        //a.concat("kumar");
+        a=a.concat("Kumar");
+        System.out.println(a);
+        /* */
+
+        String a=new String("Ankit");
+        System.out.println(a);
+
+        String b=new String("Ankit");
+        System.out.println(b);
+
+        //a.concat("kumar");
+        a=a.concat("Kumar");
+        System.out.println(a);
+
+    
+        String name="Shubham";
+//       System.out.println(name);
+//       int value =name.length();
+//       System.out.println(value);
+
+//        int value=name.length();
+//        System.out.println(value);
+//        System.out.println(name.toLowerCase());
+//        System.out.println(name.toUpperCase());
+//        System.out.println(name.length());
+//        System.out.println();
+
+
+        //String lstring = name.toLowerCase();
+        //System.out.println(lstring); 
+
+        //String ustring = name.toUpperCase();
+        //System.out.println(ustring);
+
+//        String nonTrimedString = "        shubham         ";
+//        System.out.println(nonTrimedString );
+//        String trimedString = nonTrimedString.trim();
+//        System.out.println(trimedString);
+
+        //System.out.println(nonTrimedString.trim());
+        //ystem.out.println(name.toUpperCase());
+        //System.out.println(nonTrimedString.trim());
+
+        System.out.println(name.substring(3));
+        System.out.println();
+
+
+        //System.out.println(name.substring(1));
+       System.out.println(name.substring(0,5));
+
+        //System.out.println(name.replace('h','p'));
+        //System.out.println(name.replace("shu","abc"));
+        //System.out.println(name.replace("h","abc"));
+
+
+        //System.out.println(name.startsWith("sh"));
+        //System.out.println(name.startsWith("ab"));
+        //System.out.println(name.endsWith("m"));
+
+        //System.out.println(name.charAt(1));
+        
+    }
+}
