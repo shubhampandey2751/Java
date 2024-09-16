d760c4bb10d24d78ceced87e7432ca454c3d55b7
Shubham Pandey <144239086+shubhampandey2751@users.noreply.github.com>
2024-09-16 11:43:04 +0530

    Add files via upload

---
+++ Abstractclass.java
@@ -0,0 +1,17 @@
+package Examquestion;
+abstract class animal{
+}
+class dog extends animal{
+
+}
+class Demo{
+
+}
+public class Abstractclass {
+    public static void main(String[] args) {
+        //animal r=new animal();
+        animal r=new dog(); 
+        
+    }
+    
+}
